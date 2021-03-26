bf8d2e5c38c7c722fd872c66172d07126dbe3b11
Omg <80164990+omg-urt@users.noreply.github.com>
2021-03-26 14:38:51 +0100

    Don't alocate private slots to bots.

--- code/server/sv_bot.c
+++ code/server/sv_bot.c
@@ -48,8 +48,8 @@ int SV_BotAllocateClient( void ) {
 	int			i;
 	client_t	*cl;
 
-	// find a client slot
-	for ( i = 0, cl = svs.clients; i < sv_maxclients->integer; i++, cl++ ) {
+	// find a client slot and do not allocate a private client slot to a bot
+	for ( i = sv_privateClients->integer, cl = &svs.clients[sv_privateClients->integer]; i < sv_maxclients->integer; i++, cl++ ) {
 		if ( cl->state == CS_FREE ) {
 			break;
 		}
